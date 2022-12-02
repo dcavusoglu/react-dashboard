7e6508ae7671b140e5bc4246cfd464851dc2bd48
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-12-02 11:43:33 +0300

    pie chart

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Pie as PieChart } from '../../components';
+import { pieChartData } from '../../data/dummy';
+import { Header } from '../../components'
 
 const Pie = () => {
   return (
-    <div>Pie</div>
+    <div>
+      <Header category='Pie' title='Project Cost Breakdown' />
+      <PieChart data={pieChartData} legendVisiblity height="full"/>
+    </div>
   )
 }
 
